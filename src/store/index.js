3e771dbd7915ba611b04651b203a8d8375114583
nibnualdi <nibnualdi@gmail.com>
2023-06-24 10:10:32 +0700

    add post data and get post from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
+import { axiosInstance } from '@/axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    post: null
   },
-  getters: {
-  },
+  // getters: {
+  // },
   mutations: {
+    updatePost(state, payload) {
+      state.post = payload
+    }
   },
   actions: {
+    async updatePost(context) {
+      const post = await axiosInstance.get("/post")
+      context.commit("updatePost", post.data.data)
+      console.log("post is updated : ", context.state.post)
+    }
   },
-  modules: {
-  }
+  // modules: {
+  // }
 })
