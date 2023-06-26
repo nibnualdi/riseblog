54c122daea24d642ca389348076f7ec04924d779
nibnualdi <nibnualdi@gmail.com>
2023-06-26 16:45:35 +0700

    change day to date and add separate day format function

--- src/utils/getStringDateFormat.js
+++ src/utils/getStringDateFormat.js
@@ -1,19 +1,32 @@
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+
 const getStringDateFormat = (isoString) => {
-  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+
+  const date = new Date(isoString);
+
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const dateOfMonth = date.getDate();
+
+  return `${year} ${monthNames[month+1]} ${dateOfMonth}`
+};
+
+const getStringDayFormat = (isoString) => {
 
   const date = new Date(isoString);
 
@@ -24,4 +37,4 @@ const getStringDateFormat = (isoString) => {
   return `${year} ${monthNames[month+1]} ${dayNames[day]}`
 };
 
-export { getStringDateFormat };
+export { getStringDateFormat, getStringDayFormat };
