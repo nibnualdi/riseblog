18fcff37451ab998908e4d7d298778f5b98275a4
nibnualdi <nibnualdi@gmail.com>
2023-07-01 22:22:54 +0700

    half way to finish auth

--- src/components/AuthCard.vue
+++ src/components/AuthCard.vue
@@ -18,17 +18,33 @@
       </div>
       <div class="w-full p-[200px] flex flex-col gap-[10px]">
         <h1 class="text-[48px] font-bold text-[#2B2C34]">{{ name }}</h1>
-        <input
+        <!-- signup inputs -->
+        <input v-if="name === 'Sign Up'" v-model="user.firstName"
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username" type="text" placeholder="Username" />
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password" type="password" placeholder="******************">
+          id="firstname" type="text" placeholder="firstname" />
+        <input v-if="name === 'Sign Up'" v-model="user.lastName"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="lastname" type="text" placeholder="lastname" />
+        <input v-if="name === 'Sign Up'" v-model="user.email"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email" type="text" placeholder="email" />
+        <!-- signup inputs -->
+
+        <!-- login inputs -->
+        <input v-if="name === 'Login'" v-model="idUser"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="id" type="text" placeholder="id" />
+        <!-- login inputs -->
         <button
           class="bg-[#3652E1] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button">
+          type="button"
+          @click="handleButton">
           {{ name }}
         </button>
+        <p class="text-center" v-if="name === 'Sign Up'">already have an account? <span
+            class="hover:cursor-pointer text-blue-700" @click="handleButtonToAnotherAuth('Sign Up')">login</span></p>
+        <p class="text-center" v-if="name === 'Login'">have no an account yet? <span
+            class="hover:cursor-pointer text-blue-700" @click="handleButtonToAnotherAuth('Login')">sign up</span></p>
       </div>
     </div>
   </section>
@@ -37,6 +53,9 @@
 <script>
 import Oval from "@/components/Ovals/Oval.vue"
 import Circle from "@/components/Circles/Circle.vue"
+import { useRouter } from "vue-router"
+import { ref } from "vue";
+import { axiosInstance } from "@/axios";
 
 export default {
   name: "AuthCard",
@@ -44,6 +63,36 @@ export default {
   components: {
     Oval,
     Circle
+  },
+  setup({ name }) {
+    const router = useRouter();
+    const user = ref({
+      firstName: "",
+      lastName: "",
+      email: "",
+    });
+    const idUser = ref("")
+
+    const handleButton = async () => {
+      if(name === "Sign Up") {
+        const userReq = await axiosInstance.post(`/user/create`, user.value)
+        alert(`please keep the id in mind. Because you need to put the id to login\n id = ${userReq.data.id}`)
+        console.log("user : ", userReq)
+        return
+      }
+
+      const userReq = await axiosInstance.get(`/user?created=1`)
+      const selectedUser = userReq.data.data.filter((user)=>{
+        return user.id === idUser.value
+      })
+      console.log("idUser : ", selectedUser)
+    }
+
+    const handleButtonToAnotherAuth = (name) => {
+      name === "Sign Up" ? router.push("/auth/login") : router.push("/auth/signup")
+    }
+
+    return { handleButtonToAnotherAuth, user, idUser, handleButton }
   }
 }
 </script>
