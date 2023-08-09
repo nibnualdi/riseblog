24f3419ef244d9e3f35c6baa6ab59e255f36b9cb
nibnualdi <nibnualdi@gmail.com>
2023-08-09 16:30:43 +0700

    feat: add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
-  <section class="p-[146px] flex justify-center gap-[75px]">
-    About Page
+  <section class="pt-[146px] flex flex-col justify-center gap-[75px]">
+    <img src="@/assets/about/design.jpg" alt="design">
+    <div class="p-5 lg:px-60 lg:py-16 flex flex-col gap-10 text-2xl lg:text-4xl font-thin">
+      <h1 class="text-6xl lg:text-8xl font-black">About Riseblog</h1>
+      <p class="leading-relaxed">This is a really simple blog. Here's you can post articles if you
+        already have an account. we're using <a href="https://dummyapi.io/">dummyapi.io</a> a dummy api for collecting the
+        data. So there's gonna be a lot of random data that are already made by other developers around the world and
+        because using a dummy api this blog also needs to match the data and the scheme that's provided by the api.</p>
+      <p class="leading-relaxed">It is designed by <a href="https://www.figma.com/@fadh">Fadh</a>, by using a figma, <a
+          href="https://www.figma.com/community/file/1230503106556871788/Blog-Website-Design">here's</a> if you want to
+        see the design. Also it is developed by <a href="https://github.com/nibnualdi">Ibnu</a>, by using the vue.js, <a
+          href="https://github.com/nibnualdi/blog">here's</a> if you want to see the code more...</p>
+    </div>
   </section>
 </template>
+
+<style scoped>
+a {
+  color: #3652E1;
+}
+</style>
